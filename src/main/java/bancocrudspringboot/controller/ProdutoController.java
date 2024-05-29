a286af1fa138bbd2b1c43a794a2fb51b5772d690
GabrielHvignolli <gabriel.vignolli@gmail.com>
2024-05-29 20:36:16 -0300

    ok

--- src/main/java/bancocrudspringboot/controller/ProdutoController.java
+++ src/main/java/bancocrudspringboot/controller/ProdutoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import bancocrudspringboot.model.Produto;
 import bancocrudspringboot.repository.ProdutoRepository;
 
-@CrossOrigin
+@CrossOrigin("https://b2system-one.vercel.app/")
 @RestController
 @RequestMapping("/api/v1")
 public class ProdutoController {

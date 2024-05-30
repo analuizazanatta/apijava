2f32aff762cdf92810881f409af4e6f2294da17a
GabrielHvignolli <gabriel.vignolli@gmail.com>
2024-05-29 21:31:18 -0300

    .

--- src/main/java/bancocrudspringboot/controller/ProdutoController.java
+++ src/main/java/bancocrudspringboot/controller/ProdutoController.java
@@ -1,26 +1,59 @@
 package bancocrudspringboot.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import bancocrudspringboot.exception.ResourceNotFoundException;
+import bancocrudspringboot.model.ConsultaPadrao;
 import bancocrudspringboot.model.Produto;
 import bancocrudspringboot.repository.ProdutoRepository;
 
-@CrossOrigin("https://b2system-one.vercel.app/")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class ProdutoController {
 
-    @Autowired
-    private ProdutoRepository produtoRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
-    // Listar todos os produtos
-    @GetMapping("/produto")
-    @ResponseStatus(HttpStatus.OK)
-    public List<Produto> getAll(){
-        return this.produtoRepository.findAll();
-    }
+	// Listar todos os produtos
+	@GetMapping("/produto")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Produto> getAll(){
+		return this.produtoRepository.findAll();
+	}
+
+	@PostMapping("/consultaproduto")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Produto> consultaCadastro(@Validated @RequestBody ConsultaPadrao cadastro) throws ResourceNotFoundException {
+
+		String campoUsuario = cadastro.getCampo();
+		List<Produto> listaProduto = new ArrayList<>();
+
+		switch (campoUsuario) {
+			case "id":
+				Produto produto = produtoRepository.findById(Long.parseLong(cadastro.getValor1()))
+						.orElseThrow(() -> new ResourceNotFoundException("Registro não encontrado para o ID :: " + cadastro.getValor1()));
+
+				listaProduto.add(produto);
+				break;
+			// case "email":
+			// 	listaUsuario = this.usuarioRepository.findUsuarioByEmail(cadastro.getValor1());
+			// 	break;
+			// case "nome":
+			// 	listaUsuario = this.usuarioRepository.findUsuarioByNome(cadastro.getValor1());
+			// 	break;
+			default:
+				// implementar campo nao existente
+				// listaProduto.orElseThrow(() ->  new ResourceNotFoundException("Campo inexistente na tabela do banco de dados!" + cadastro.getCampo()));
+				break;
+		}
+
+		return listaProduto;
+	}
 }
